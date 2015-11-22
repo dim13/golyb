72b486c4f7bd29471725d72318350c71bc24ff4d
Dimitri Sokolyuk <demon@dim13.org>
2015-11-22 23:39:55 +0100

    Split files

--- parse.go
+++ parse.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"io/ioutil"
 )
@@ -70,46 +69,4 @@ func ParseFile(fname string) (Program, error) {
 
 func ParseString(prog string) Program {
 	return parse(bytes.NewBufferString(prog))
-}
-
-func printArg(n int, more, less string) string {
-	var s string
-	for i := 0; i < n; i++ {
-		s += more
-	}
-	for i := 0; i > n; i-- {
-		s += less
-	}
-	return s
-}
-
-func (p Program) String() string {
-	var s string
-	for _, cmd := range p {
-		s += fmt.Sprint(cmd)
-	}
-	return s
-}
-
-func (c Command) String() string {
-	var s string
-	switch c.Op {
-	case Add:
-		s = printArg(c.Arg, "+", "-")
-	case Move:
-		s = printArg(c.Arg, ">", "<")
-	case Print:
-		s = "."
-	case Scan:
-		s = ","
-	case BNZ:
-		s = "[" + fmt.Sprint(c.Branch) + "]"
-	case Clear:
-		s = "[-]"
-	case Mult:
-		s = "[-" + printArg(c.Off, ">", "<") +
-			printArg(c.Arg, "+", "-") +
-			printArg(-c.Off, ">", "<") + "]"
-	}
-	return s
 }
