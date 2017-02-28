403b7beb4240d6154e794f4859ed6309249067f7
Dimitri Sokolyuk <demon@dim13.org>
2017-02-28 09:24:02 +0100

    Print sign

--- dump.go
+++ dump.go
@@ -27,13 +27,13 @@ func (c Command) String() string {
 	buf := new(bytes.Buffer)
 	fmt.Fprintf(buf, "%s", c.Op)
 	if c.Arg != 0 {
-		fmt.Fprintf(buf, " %d", c.Arg)
+		fmt.Fprintf(buf, " %+d", c.Arg)
 	}
 	if c.Off != 0 {
-		fmt.Fprintf(buf, " @%d", c.Off)
+		fmt.Fprintf(buf, " @%+d", c.Off)
 	}
 	if c.Dst != 0 {
-		fmt.Fprintf(buf, " → @%d", c.Dst)
+		fmt.Fprintf(buf, " → @%+d", c.Dst)
 	}
 	return buf.String()
 }
